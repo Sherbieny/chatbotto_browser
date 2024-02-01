b09ede345c88839b09f8e8ebf42f67039d862c1a
sherbieny <sherbieny@osakabiso.jp>
2024-02-01 16:48:21 +0900

    Change path of admin page to absolute path of root page, trial for github io

--- js/index.js
+++ js/index.js
@@ -33,7 +33,7 @@ document.addEventListener('DOMContentLoaded', async function () {
             {
                 name: 'admin',
                 path: '/admin/',
-                url: './admin.html',
+                url: '/admin.html',
             },
         ],
         // loader
